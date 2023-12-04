f42cd267a773f5dc49b7fc4b224b5cc2991ef663
No <87939631+Zayk0oo@users.noreply.github.com>
2023-12-04 12:03:22 +0100

    Add files via upload

--- Mon site WEB V2/js/custom.js
+++ Mon site WEB V2/js/custom.js
@@ -1,4 +1,3 @@
-// to get current year
 function getYear() {
     var currentDate = new Date();
     var currentYear = currentDate.getFullYear();
@@ -7,11 +6,10 @@ function getYear() {
 
 getYear();
 
-/** google_map js **/
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
+        center: new google.maps.LatLng(47.9557, 0.2076),
+        zoom: 15,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
